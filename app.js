810640dfb9f4a86f4d528f5029d25d9eb3da78ef
PatrickBoynton <jpboynton3@gmail.com>
2021-05-22 19:57:16 -0400

    Initial Commit.

---
+++ app.js
@@ -0,0 +1,56 @@
+const today = new Date();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const categories = [
+  { Astronomy: "purple" },
+  { Travel: "red" },
+  { Business: "blue" },
+];
+
+const card = document.createElement("div");
+const time = document.createElement("time");
+const img = document.createElement("img");
+const span = document.createElement("span");
+const h3 = document.createElement("h3");
+const p = document.createElement("p");
+const test = document.querySelector(".test");
+
+card.classList.add("card");
+span.classList.add("category");
+h3.classList.add("title");
+
+time.innerHTML =
+  months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+
+card.appendChild(time);
+card.appendChild(img);
+card.appendChild(span);
+card.appendChild(h3);
+card.appendChild(p);
+
+fetch("/assets/articles.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.map((x) => {
+      span.innerHTML = x.category;
+      h3.innerHTML = x.title;
+      p.innerHTML = x.article;
+      img.src = "/assets/Andromeda.jpeg";
+      img.alt = "Dynamic Image";
+
+      document.body.appendChild(card);
+    });
+  });
