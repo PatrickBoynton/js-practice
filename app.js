973e219f76a9e67ae5944c67b206c05a5d970983
PatrickBoynton <jpboynton3@gmail.com>
2021-05-23 13:53:26 -0400

    Moved the card creation inside the map.

--- app.js
+++ app.js
@@ -20,36 +20,40 @@ const categories = [
   { Business: "blue" },
 ];
 
-const card = document.createElement("div");
-const time = document.createElement("time");
-const img = document.createElement("img");
-const span = document.createElement("span");
-const h3 = document.createElement("h3");
-const p = document.createElement("p");
-const test = document.querySelector(".test");
-
-card.classList.add("card");
-span.classList.add("category");
-h3.classList.add("title");
-
-time.innerHTML =
-  months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
-
-card.appendChild(time);
-card.appendChild(img);
-card.appendChild(span);
-card.appendChild(h3);
-card.appendChild(p);
-
 fetch("/assets/articles.json")
   .then((response) => response.json())
   .then((data) => {
     data.map((x) => {
+      const card = document.createElement("div");
+      const time = document.createElement("time");
+      const img = document.createElement("img");
+      const span = document.createElement("span");
+      const h3 = document.createElement("h3");
+      const p = document.createElement("p");
+      const test = document.querySelector(".test");
+
+      card.classList.add("card");
+      span.classList.add("category");
+      h3.classList.add("title");
+
+      time.innerHTML =
+        months[today.getMonth()] +
+        " " +
+        today.getDate() +
+        ", " +
+        today.getFullYear();
+
       span.innerHTML = x.category;
       h3.innerHTML = x.title;
       p.innerHTML = x.article;
       img.src = "/assets/Andromeda.jpeg";
       img.alt = "Dynamic Image";
+
+      card.appendChild(time);
+      card.appendChild(img);
+      card.appendChild(span);
+      card.appendChild(h3);
+      card.appendChild(p);
 
       document.body.appendChild(card);
     });
